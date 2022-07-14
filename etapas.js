7b56fae9d737683ca5ffa446ebb393b6fdad09b0
Marco-coutinho <mrco_coutinho@hotmail.com>
2022-07-14 17:26:19 -0300

    master

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [ 
+    {
+        titulo: 'Vereador',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name:'Fulano dali',
+                partdo: 'ABC',
+                fotos: [
+                    {url:'3811.jgp', legenda: 'Vereaador'}
+                ]
+            },
+            {
+                numero:77222,
+                name: 'Beltrano de tau',
+                partido: 'Paulista',
+                fotos: [
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name:'claudio',
+                partido: 'Anarquista',
+                vice:'siclano',
+                fotos: [
+                    {url:'99.jpg', legenda:'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                name: 'zuckem',
+                partido:'capitalista',
+                vice: 'zuleide',
+                fotos : [
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+        ]
+    }
+];
